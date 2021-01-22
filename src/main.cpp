1effcaf45442475b3cf8c52824c3f315ffbaf5c0
Glenn Hazelwood <hazelnutvt04@gmail.com>
2021-01-22 01:15:11 -0500

    Fixed flattened_vals()

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,11 @@
 //#include <algorithm>
 #include "my_utils.hpp"
 
-void sort_nine( unsigned int* sorted, const unsigned int* vals, const bool& debug = false ) {
+constexpr int NRows = 4;
+constexpr int NColumns = 9;
+constexpr int NItems = NRows * NColumns;
+
+void sort_subset( unsigned int* sorted, const unsigned int* vals, const int& num_to_sort, const bool& debug = false ) {
    try {
       if (!vals) {
          throw std::runtime_error( "vals is nullptr." );
@@ -14,14 +18,14 @@ void sort_nine( unsigned int* sorted, const unsigned int* vals, const bool& debu
       }
 
       if ( debug ) {
-         print_vals<unsigned int>( vals, 9, "Vals ", ", ", "\n" );
+         print_vals<unsigned int>( vals, num_to_sort, "Vals ", ", ", "\n" );
       }
 
-      std::copy( vals, vals + 9, sorted );
-      std::sort( sorted, sorted + 9 );
+      std::copy( vals, vals + num_to_sort, sorted );
+      std::sort( sorted, sorted + num_to_sort );
       
       if ( debug ) {
-         print_vals<unsigned int>( sorted, 9, "Sorted ", ", ", "\n" );
+         print_vals<unsigned int>( sorted, num_to_sort, "Sorted ", ", ", "\n" );
       }
 
    } catch ( std::exception& ex ) {
@@ -30,27 +34,24 @@ void sort_nine( unsigned int* sorted, const unsigned int* vals, const bool& debu
 }
 
 
-void flatten_by_nine( unsigned int* flattened_vals, const unsigned int** vals, 
+void flatten_by_nrows( unsigned int* flattened_vals, const unsigned int vals[][9], 
       const int& num_rows, const bool& debug = false ) {
 
    try {
-      if (!vals) {
-         throw std::runtime_error( "vals is nullptr." );
-      }
       if (!flattened_vals) {
          throw std::runtime_error( "flattened_vals is nullptr." );
       }
 
       if ( debug ) {
          for( int row_index = 0; row_index < num_rows; ++row_index ) {
-            print_vals<unsigned int>( &(vals[row_index][0]), 9, "Vals ", ", ", "\n" );
+            print_vals<unsigned int>( &(vals[row_index][0]), NColumns, "Vals ", ", ", "\n" );
          } 
       }
       
-      for( int row_index = 0; row_index < num_rows; ++row_index ) {
-         //std::copy( vals[row_index], vals[row_index] + 9, &(flattened_vals[row_index * 9]) );
-         for( int col_index = 0; col_index < 9; ++col_index ) {
-            flattened_vals[row_index * 9 + col_index] = vals[row_index][col_index];
+      for( int row_index = 0; row_index < NRows; ++row_index ) {
+         //std::copy( vals[row_index], vals[row_index] + NColumns, &(flattened_vals[row_index * 9]) );
+         for( int col_index = 0; col_index < NColumns; ++col_index ) {
+            flattened_vals[row_index * NColumns + col_index] = vals[row_index][col_index];
          } 
       }
 
@@ -64,22 +65,25 @@ void flatten_by_nine( unsigned int* flattened_vals, const unsigned int** vals,
 
 }
 
+
 int main( int argc, char** argv ) {
    try {
       
-      unsigned int vals[4][9] = {
+      unsigned int vals[NRows][NColumns] = {
          { 9, 4, 7, 2, 3, 6, 5, 8, 1 },
          { 2, 8, 4, 7, 1, 5, 6, 3, 9 },
          { 1, 5, 6, 3, 9, 2, 8, 4, 7 }, 
          { 5, 1, 7, 6, 3, 2, 9, 8, 4 }
       };
-      unsigned int sorted[4][9];
-      unsigned int flattened_vals[36];
+      unsigned int flattened_vals[NItems];
+      unsigned int sorted_flattened_vals[NItems];
       bool debug = true;
+
+      flatten_by_nrows( &(flattened_vals[0]), vals, NRows, debug );
       
-      flatten_by_nine( &(flattened_vals[0]), &(*vals[0]), debug );
-      sort_nine( &(sorted[0][0]), &(vals[0][0]), debug );
-      sort_nine( &(sorted[1][0]), &(vals[1][0]), debug );
+      for( int row_index = 0; row_index < NRows; ++row_index ) {
+         sort_subset( &(sorted_flattened_vals[row_index * NColumns]), &(flattened_vals[row_index * NColumns]), NColumns, debug );
+      } 
 
       return EXIT_SUCCESS;
 
