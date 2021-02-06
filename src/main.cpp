77b7bb015aa3c1f577e714e4e57c11cfe3e56ebf
Glenn Hazelwood <hazelnutvt04@gmail.com>
2021-02-05 21:19:45 -0500

    Added the invalid solution example for testing

--- src/main.cpp
+++ src/main.cpp
@@ -107,6 +107,26 @@ int main( int argc, char** argv ) {
    } else {
       std::cout << "Sodoku solution 'valid_vals' is NOT valid\n"; 
    }
+   
+   unsigned int invalid_vals[NRows][NColumns] = {
+      {5, 3, 4, 6, 7, 8, 9, 1, 2}, 
+      {6, 7, 2, 1, 9, 0, 3, 4, 8},
+      {1, 0, 0, 3, 4, 2, 5, 6, 0},
+      {8, 5, 9, 7, 6, 1, 0, 2, 0},
+      {4, 2, 6, 8, 5, 3, 7, 9, 1},
+      {7, 1, 3, 9, 2, 4, 8, 5, 6},
+      {9, 0, 1, 5, 3, 7, 2, 1, 4},
+      {2, 8, 7, 4, 1, 9, 6, 3, 5},
+      {3, 0, 0, 4, 8, 1, 1, 7, 9}
+   };
+
+   if ( validSolution( invalid_vals ) ) {
+      std::cout << "Sodoku solution 'invalid_vals' is valid\n"; 
+      std::cout << "This is NOT expected\n"; 
+   } else {
+      std::cout << "Sodoku solution 'invalid_vals' is NOT valid\n"; 
+      std::cout << "This is expected\n"; 
+   }
 
    return EXIT_SUCCESS;
 
